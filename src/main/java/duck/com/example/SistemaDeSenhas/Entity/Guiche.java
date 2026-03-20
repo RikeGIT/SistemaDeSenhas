7dca4cb448983d7ccb10501d030fc2488a4d6ea9
Renata Medeiros <114954319+RenataMedeir0s@users.noreply.github.com>
2026-03-19 22:47:21 -0300

    Add files via upload

---
+++ src/main/java/duck/com/example/SistemaDeSenhas/Entity/Guiche.java
@@ -0,0 +1,25 @@
+package duck.com.example.SistemaDeSenhas.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "guiches")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Guiche {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer numero;
+
+    private String setor;
+}
