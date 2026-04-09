0cfefdce59f5f045b5a33c6a9e80135c77d47b11
Renata Medeiros <114954319+RenataMedeir0s@users.noreply.github.com>
2026-04-08 23:26:59 -0300

    AtendimentoController.java

--- src/main/java/duck/com/example/SistemaDeSenhas/controller/AtendimentoController.java
+++ src/main/java/duck/com/example/SistemaDeSenhas/controller/AtendimentoController.java
@@ -1,10 +1,13 @@
 package duck.com.example.SistemaDeSenhas.controller;
 
 import duck.com.example.SistemaDeSenhas.entity.Atendimento;
+import duck.com.example.SistemaDeSenhas.repository.AtendimentoRepository;
 import duck.com.example.SistemaDeSenhas.service.AtendimentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/atendimentos")
@@ -12,6 +15,9 @@ public class AtendimentoController {
 
     @Autowired
     private AtendimentoService atendimentoService;
+
+    @Autowired
+    private AtendimentoRepository atendimentoRepository;
 
     @PostMapping("/chamar/{guicheId}")
     public ResponseEntity<Atendimento> chamar(@PathVariable Long guicheId) {
@@ -28,6 +34,25 @@ public class AtendimentoController {
         if (atendimento == null) {
             return ResponseEntity.noContent().build();
         }
+        return ResponseEntity.ok(atendimento);
+    }
+
+    // NOVO ENDPOINT PARA O PAINEL
+    @GetMapping("/painel")
+    public List<Atendimento> painel() {
+        return atendimentoRepository.findByDataHoraFimIsNull();
+    }
+
+    // NOVO ENDPOINT PARA RECHAMAR SENHA
+    @PostMapping("/rechamar/{id}")
+    public ResponseEntity<Atendimento> rechamar(@PathVariable Long id){
+
+        Atendimento atendimento = atendimentoService.rechamar(id);
+
+        if(atendimento == null){
+            return ResponseEntity.notFound().build();
+        }
+
         return ResponseEntity.ok(atendimento);
     }
 }
